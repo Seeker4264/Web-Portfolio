18588de3e8298ca4e2e53958506c2a91331e4496
Seeker4264 <sebashmorales@gmail.com>
2025-07-26 17:31:02 -0400

    remake of the entire website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'es'],
-    routing: {
-      prefixDefaultLocale: true
-    }
-  }
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
